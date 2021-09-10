b271f07645d0a8d165f13a4d227cb32949e034a3
miz-ab <mizaby19@gmail.com>
2021-09-10 15:54:54 +0100

    ''

--- consumer.py
+++ consumer.py
@@ -1,9 +1,16 @@
 from kafka import KafkaConsumer
+from helper.helper import show_list_of_messages_in_topic
 
 
+'''
 TOPIC_NAME = 'TestTopic'
 
 consumer = KafkaConsumer(TOPIC_NAME)
 print(consumer.topics())
 for message in consumer:
     print(message)
+    
+'''
+
+show_list_of_messages_in_topic()
+
--- helper/helper.py
+++ helper/helper.py
@@ -2,11 +2,15 @@ from kafka import KafkaConsumer
 
 TOPIC_NAME = 'TestTopic'
 
+
 def show_list_of_topics():
     consumer = KafkaConsumer(TOPIC_NAME)
     print(consumer.topics())
 
+
 def show_list_of_messages_in_topic():
     consumer = KafkaConsumer(TOPIC_NAME)
+    message_list = []
     for message in consumer:
-        print(message.value)
+        message_list.append(message.value)
+    print(message_list)
